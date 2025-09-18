4ee4e37f36f0908fd1aaf77b2456287fc56dea2a
flaviocalaca <flaviopereiralc@gmail.com>
2025-09-18 14:40:24 -0300

    atualizando links e foto de perfil

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ function toggleMode() {
   // substituir a imagem
   if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/avatar-light.jpg")
   } else {
     // se tiver sem light mode, manter imagem normal
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/avatar.jpg")
   }
 }
  
